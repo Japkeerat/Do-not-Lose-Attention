2427489b7ef44c3a64635c4e83f8bf3fb18497c2
Japkeerat Singh <japkeerat21@gmail.com>
2023-04-05 15:08:26 +0530

    Unblocking

--- blocked.js
+++ blocked.js
@@ -16,4 +16,30 @@ function getQueryParam(param) {
   }
   
   updateBlockedMessage();
+
+function unblockDomain(domain) {
+  const domainWithoutWWW = domain.replace(/^www\./, '');
+  const domainWithWWW = 'www.' + domainWithoutWWW;
+
+  chrome.storage.sync.get('blockedWebsites', (data) => {
+    const blockedWebsites = data.blockedWebsites || [];
+    const updatedBlockedWebsites = blockedWebsites.filter(
+      (blocked) => blocked !== domainWithoutWWW && blocked !== domainWithWWW
+    );
+
+    chrome.storage.sync.set({ blockedWebsites: updatedBlockedWebsites }, () => {
+      window.location.href = `https://${domain}`;
+    });
+  });
+}
+
+function unblockButtonClickHandler() {
+    const domain = getQueryParam('domain');
+    if (domain) {
+      unblockDomain(domain);
+    }
+  }
   
+document.addEventListener('DOMContentLoaded', () => {
+document.getElementById('unblock-button').addEventListener('click', unblockButtonClickHandler);
+});
